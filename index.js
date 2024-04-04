e62fc8507fe8fdc957b7946a357c84806c8bc8a0
Max <jvmax1907@gmail.com>
2024-04-04 15:34:04 -0300

    Redis + ajustes

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
 
+const redisClient = require('redis')
+const cl = redisClient.createClient();
+cl.connect();
+cl.on('error', (err) => {
+  console.log('Error ' + err);
+});
 const app = express();
+app.use(express.static('public'));
+
 const server = createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '/public/index.html'));
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+io.on('connection',async (socket) => {
+  const messages = await cl.lRange('messages', 0, -1);
+  const parsedMessages = messages.map((msg) => JSON.parse(msg));
+  socket.emit("existentes", parsedMessages);
+  
+  socket.on('chat message', (msg, user) => {
+    io.emit('chat message', msg, user);
+    cl.rPush('messages', JSON.stringify({ msg, user }));
   });
 });
 
--- public/script.js
+++ public/script.js
@@ -3,18 +3,27 @@ const socket = io();
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
+let user = prompt("Digite seu nome");
 
 form.addEventListener('submit', (e) => {
   e.preventDefault(); 
   if (input.value) {
-      socket.emit('chat message', input.value);
+      socket.emit('chat message', input.value, user);
       input.value = '';
   }
 }); 
 
-socket.on('chat message', (msg) => {
+socket.on('chat message', (msg,usuario) => {
   const item = document.createElement('li');
-  item.textContent = msg;
+  item.textContent = `${usuario}: ${msg}`;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('existentes', (msgs) => {
+  msgs.forEach((msg) => {
+    const item = document.createElement('li')
+    item.textContent = `${msg.user}: ${msg.msg}`;
+    messages.appendChild(item);
+  });
 });
